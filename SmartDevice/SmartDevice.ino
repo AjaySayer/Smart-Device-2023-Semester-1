b4b24122bda659da7e8deaa740c3292304da5e2d
AjaySayer <0852351@schoolsnet.act.edu.au>
2023-05-22 10:12:49 +1000

    SD card stuff
    yes

--- SmartDevice/SmartDevice.ino
+++ SmartDevice/SmartDevice.ino
@@ -10,6 +10,13 @@ DateTime rightNow;  // used to store the current time.
 // SD Card - Confirm Pin
 #define SDpin 53
 
+//DC Motor 
+int E1 = 6;
+int M1 = 7;
+
+//Potentiometer
+#define pot A3
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);  // Open serial communications and wait for port to open:
@@ -21,79 +28,85 @@ void setup() {
   Serial.print("Initializing SD card...");
   if (!SD.begin(SDpin)) {
     Serial.println("initialization failed!");
-    while (1)
-      ;
+    while (1);
+    
   }
+
+  // Real Time Clock (RTC)
+  rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
+  Serial.println("initialization done.");
+  logEvent("System Initialisation...");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
 
 
-  
+
+
   delay(250);
 }
 
 /*
- The user controls the speed of the engine (DC Motor) using the potentiometer (Accelerator)
+  The user controls the speed of the engine (DC Motor) using the potentiometer (Accelerator)
 
- @params none
- @returns none
- */
- 
-void engineSpeed(){
-  
+  @params none
+  @returns none
+*/
+
+void engineSpeed() {
+
 }
 
 
 /*
- The Sonar will activate the Ejector seat (Servo Motor)
+  The Sonar will activate the Ejector seat (Servo Motor)
 
- @params none
- @returns none
- */
+  @params none
+  @returns none
+*/
 
 void ejectorSeat() {
 
-  
+
 }
 
 
 /*
- The line sensor will turn the engine (DC Motor) on or off
+  The line sensor will turn the engine (DC Motor) on or off
 
- @params none
- @returns none
- */
+  @params none
+  @returns none
+*/
 
 void engineStart() {
 
-  
+
 }
 
 
 /*
- The GPS will track the wheelchair and use the traffic lights to do stuff
+  The GPS will track the wheelchair and use the traffic lights to do stuff
 
- @params none
- @returns none
- */
+  @params none
+  @returns none
+*/
 
 void trackingSystem() {
 
-  
+
 }
 
 
 /*
- The self destruct button (Crash Sensor) will blow up the Vehicle (Piezo)
+  The self destruct button (Crash Sensor) will blow up the Vehicle (Piezo)
 
- @params none
- @returns none
- */
+  @params none
+  @returns none
+*/
 
 void selfDestruct() {
 
-  
+
 }
